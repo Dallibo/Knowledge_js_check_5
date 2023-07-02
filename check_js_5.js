9eefc58ce380a7fb880348efd810463e6b383fe6
Dallibo <denismalyarenko1@gmail.com>
2023-07-02 20:28:14 +0300

    2

--- check_js_5.js
+++ check_js_5.js
@@ -11,3 +11,51 @@ class PrintMachine {
   }
   const printer = new PrintMachine(16, 'blue', 'Tahoma, Arial, sans-serif;');
   printer.print('Hellow');
+
+
+  class NewsString {
+    constructor() {
+      this.newsArray = [];
+    }
+      get count() {
+      return this.newsArray.length;
+    }
+    displayAllNews() {
+      this.newsArray.forEach(news => console.log(news));
+    }
+    addNews(news) {
+      this.newsArray.push(news);
+    }
+    removeNews(news) {
+      const index = this.newsArray.indexOf(news);
+      if (index !== -1) {
+        this.newsArray.splice(index, 1);
+      }
+    }
+    sortNewsByDate() {
+      this.newsArray.sort((a, b) => b.date - a.date);
+    }
+  
+    searchNewsByTag(tag) {
+      return this.newsArray.filter(news => news.tags.includes(tag));
+    }
+}
+
+let newsString = new NewsString();
+newsString.addNews({
+  title: "News 1",
+  text: "Новость 1",
+  tags: ["tag1", "tag2"],
+  date: new Date(2023, 5, 28, 4, 26, 45, 456)
+});
+
+newsString.addNews({
+  title: "News 2",
+  text: "Новость 2",
+  tags: ["tag2", "tag3"],
+  date: new Date(2023, 5, 29, 12, 30, 0, 0)
+});
+
+console.log("Number of news:", newsString.count);
+newsString.displayAllNews();
+
